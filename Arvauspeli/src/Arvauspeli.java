fb7e9c1eb811efa994c784035fb5c887ff37e277
Oisteri <oskari.ahde@gmail.com>
2024-09-07 14:00:45 +0300

    do/while loop alku

--- Arvauspeli/src/Arvauspeli.java
+++ Arvauspeli/src/Arvauspeli.java
@@ -8,7 +8,21 @@ public class Arvauspeli {
         String input = "";
         String correctname = "Aatu";
 
-        System.out.println("Guess my name");
-        input = in.nextLine();
+
+            do {
+                System.out.println("Guess my name");
+                input = in.nextLine();
+
+                if (input.equalsIgnoreCase(correctname)) 
+                    {
+                        System.out.println("Congratulations! You guessed correctly!");
+                    }
+                else 
+                    {
+                        System.out.println("Wrong! You guessed incorrectly, guess again!");
+                    }
+
+            } while (!input.equals(correctname));
+        
     }
 }
